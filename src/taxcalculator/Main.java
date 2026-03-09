02c9ce8624124238c258d2697d0dc697b25ae212
GateWay Software <juliantheblaze@gmail.com>
2026-03-09 11:24:07 -0400

    Add src/taxcalculator/Main.java

---
+++ src/taxcalculator/Main.java
@@ -0,0 +1,9 @@
+package taxcalculator;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(TaxCalculatorApp.class, args);
+    }
+}
